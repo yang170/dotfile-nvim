e9de92b77a4a8b5203797bb54e82ed4f3c3476b7
Yang Jiang <jyang96069@gmail.com>
2023-10-15 17:06:41 -0700

    update key mapping

--- lua/plugin-configs/vim-oscyank-config.lua
+++ lua/plugin-configs/vim-oscyank-config.lua
@@ -1,3 +1,3 @@
-vim.keymap.set('n', '<leader>c', '<Plug>OSCYankOperator')
-vim.keymap.set('n', '<leader>cc', '<leader>c_', {remap = true})
-vim.keymap.set('v', '<leader>c', '<Plug>OSCYankVisual')
+vim.keymap.set('n', '<leader>y', '<Plug>OSCYankOperator')
+vim.keymap.set('n', '<leader>yy', '<leader>c_', {remap = true})
+vim.keymap.set('v', '<leader>y', '<Plug>OSCYankVisual')
